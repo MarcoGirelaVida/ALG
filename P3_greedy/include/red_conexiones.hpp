77833e86178d0ecf3e11634321cf14b4b212ffef
Marco_Girela <marcogirelavida@gmail.com>
2024-04-11 18:46:46 +0200

    Template for P3 uploaded

---
+++ P3_greedy/include/red_conexiones.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+#ifndef red_conexiones
+#define red_conexiones
+
+// Definición de funciones
+
+// Definición de variables
+
+#endif
---
+++ P3_greedy/src/red_conexiones.cpp
@@ -0,0 +1,6 @@
+/*
+Marco Girela Vida 2ºB3
+
+Implementación de djikstra en c++
+*/
+#include "red_conexiones.hpp"
